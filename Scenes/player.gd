bd9cf7c00a6a301c84c360927529c1a36a007915
RomSTil <dintermaksim5@gmail.com>
2024-12-10 02:01:41 +0300

    добавил анимацию простоя

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,26 +1,26 @@
 extends CharacterBody2D
 
-
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Добавляем гравитацию.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Прыжок.
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Движение влево/вправо.
 	var direction := Input.get_axis("left", "right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED
-		$CollisionShape2D/AnimatedSprite2D.flip_h = true
+		$CollisionShape2D/AnimatedSprite2D.flip_h = direction < 0
 		$CollisionShape2D/AnimatedSprite2D.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		$CollisionShape2D/AnimatedSprite2D.play("idle")
 
+	# Движение персонажа.
 	move_and_slide()
